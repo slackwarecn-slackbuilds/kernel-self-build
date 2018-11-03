7b689296a9c2ce967b798721c8ef2b9317f3cb50
mdrights <mdrights@tutanota.de>
2018-11-03 20:05:54 +0800

    updated to 4.19(.0); customise a config item for Apparmor.

--- doinst.sh
+++ doinst.sh
@@ -1,6 +1,6 @@
 ( cd boot/ ; rm -rf vmlinuz )
-( cd boot/ ; ln -sf vmlinuz-generic-4.9.113 vmlinuz )
+( cd boot/ ; ln -sf vmlinuz-generic-4.19.0 vmlinuz )
 
 ( cd boot/ ; rm -rf System.map )
-( cd boot/ ; ln -sf System.map-4.9.113 System.map )
+( cd boot/ ; ln -sf System.map-4.19.0 System.map )
 
