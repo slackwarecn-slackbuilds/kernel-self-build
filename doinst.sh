925f8b3bcde9020998b72d36f5372b9f7f23d5be
mdrights <mdrights@tutanota.de>
2021-01-01 14:39:34 +0800

    updated to 5.10.4; bugfix in doinst.sh

--- doinst.sh
+++ doinst.sh
@@ -1,6 +1,9 @@
 ( cd boot/ ; rm -rf vmlinuz )
-( cd boot/ ; ln -sf vmlinuz-generic-4.19.0 vmlinuz )
+( cd boot/ ; ln -sf vmlinuz-generic-5.10.4 vmlinuz )
 
 ( cd boot/ ; rm -rf System.map )
-( cd boot/ ; ln -sf System.map-4.19.0 System.map )
+( cd boot/ ; ln -sf System.map-5.10.4 System.map )
 
+( cd lib/modules/5.10.4 ; rm -rf build source)
+( cd lib/modules/5.10.4 ; ln -s usr/src/linux-5.10.4 build )
+( cd lib/modules/5.10.4 ; ln -s usr/src/linux-5.10.4 source )
